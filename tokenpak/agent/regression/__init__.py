541140ada5168e67ca311d6431f4ecc7fa412430
Kevin Yang <hello@repliflow.com>
2026-03-10 13:45:03 -0700

    feat(regression): add component-level artifact reuse planning

--- tokenpak/agent/regression/__init__.py
+++ tokenpak/agent/regression/__init__.py
@@ -1,3 +1,12 @@
+from .artifact_reuse import (
+    ArtifactReusePlan,
+    ComponentComparison,
+    compare_components,
+    merge_artifact_components,
+    plan_artifact_reuse,
+    split_artifact_components,
+    validate_merged_artifact,
+)
 from .feature_detector import (
     FeatureDetectionReport,
     FeatureResult,
@@ -10,4 +19,11 @@ __all__ = [
     "FeatureDetectionReport",
     "detect_feature_regressions",
     "build_targeted_repair_prompt",
+    "ComponentComparison",
+    "ArtifactReusePlan",
+    "split_artifact_components",
+    "compare_components",
+    "plan_artifact_reuse",
+    "merge_artifact_components",
+    "validate_merged_artifact",
 ]
