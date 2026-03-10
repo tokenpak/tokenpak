49806bf696dc55a7a5bb9135d802b260333d3462
Kevin Yang <hello@repliflow.com>
2026-03-10 12:35:23 -0700

    add progressive disclosure pipeline for ingest retrieval

--- tokenpak/agent/ingest/__init__.py
+++ tokenpak/agent/ingest/__init__.py
@@ -2,5 +2,12 @@
 
 from .api import create_ingest_app
 from .api import router as ingest_router
+from .disclosure import build_disclosure_payload, choose_disclosure_level, shortlist_sections
 
-__all__ = ["create_ingest_app", "ingest_router"]
+__all__ = [
+    "create_ingest_app",
+    "ingest_router",
+    "choose_disclosure_level",
+    "shortlist_sections",
+    "build_disclosure_payload",
+]
